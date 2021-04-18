85c36819f7a3d28c0c5de806a45d4a0940639856
陈镇雨 <2269283832@qq.com>
2021-04-18 11:01:01 +0800

    更新了一下

---
+++ src/main/java/mybatisTest/dao/EmployeeMapper.java
@@ -0,0 +1,11 @@
+package mybatisTest.dao;
+
+
+import mybatisTest.bean.Employee;
+
+public interface EmployeeMapper {
+
+	public Employee getEmpById(Integer id);
+//	public Employee getEmpBylastname(String lastname);
+
+}
